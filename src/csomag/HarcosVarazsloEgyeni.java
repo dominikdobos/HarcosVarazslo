cd42f3fa0d3ac90c231b16b93e69be221541623f
Dobos Dominik (SZF_2023_2025) <dobos.dominik@diak.szamalk-szalezi.hu>
2024-02-20 10:45:14 +0100

    kezdet

---
+++ src/csomag/HarcosVarazsloEgyeni.java
@@ -0,0 +1,14 @@
+
+package csomag;
+
+import java.util.Random;
+
+public class HarcosVarazsloEgyeni {
+
+    static Random rnd = new Random();
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
